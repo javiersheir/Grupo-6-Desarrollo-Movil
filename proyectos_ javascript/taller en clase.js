83848d4f59fe24eb09ff3d7cf7b0670fcfbd3170
javiersheir <90363628+javiersheir@users.noreply.github.com>
2021-09-19 23:35:55 -0500

    Sprint 2
    Se creó una carpeta llamada Proyecto Ciclo 3 y se creará una carpeta con cada Sprint para poder visualizar los avances.

---
+++ proyectos_ javascript/taller en clase.js
@@ -0,0 +1,11 @@
+(function(){
+    "use strict";
+    /* Inicio de tu código */
+    function greetMe(tuNombre) {
+      alert('Hola ' + tuNombre);
+    }
+  
+    greetMe('Mundo');
+    console.log("Hola Javier")
+    /* Fin de tu código */
+  })();
